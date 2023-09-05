c64d41b944086fff9a250783bbe0ba346b56eadb
aarav5feb <103936756+aarav5feb@users.noreply.github.com>
2023-09-05 17:50:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+
+
+function preload(){
+}
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized!');
+}
+
+
+function draw(){
+    background('green');
+    textSize(difference);
+    fill('#F90093');
+    text('Aarav',50,400)
+}
+
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference =floor(leftWristX - rightWristX);
+        console.log("leftWristX = "+ leftWristX + "rightWristX= "+ rightWristX + "difference ="+difference);
+    
+    }
+}
